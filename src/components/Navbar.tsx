d9d933d18fcc8ae3d43da99ded58a581803a85d5
Xue <xue.contact@gmail.com>
2024-05-24 17:47:58 -0700

    Mobile navBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 // import { Link } from "react-router-dom";
 import { HashLink as Link } from "react-router-hash-link";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 interface TitleProps {
   title: string;
@@ -13,6 +14,11 @@ interface NavbarProps {
 interface navItemsType {
   title: string;
   url: string;
+}
+
+interface MobileNavProps {
+  isOpen?: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
 }
 
 const navItems: navItemsType[] = [
@@ -54,31 +60,43 @@ const subNavItems: navItemsType[] = [
 ];
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <div className="fixed z-50 flex w-full flex-row items-center justify-center p-4">
-      <Title title="Xue Hua" />
-      <Navigation items={navItems} />
-    </div>
+    <>
+      <div className="fixed  z-50 hidden w-full flex-row items-center justify-center p-4 md:flex">
+        <Title title="Xue Hua" />
+        <Navigation items={navItems} />
+      </div>
+      {isOpen ? (
+        <div className="fixed z-50">
+          <MobileNav setIsOpen={setIsOpen} />
+        </div>
+      ) : (
+        <div className="fixed right-4 top-4  z-50 ">
+          <MobileNavBtn isOpen={isOpen} setIsOpen={setIsOpen} />
+        </div>
+      )}
+    </>
   );
 };
 
 const Title: React.FC<TitleProps> = ({ title }) => {
   return (
-    <h1 className="flex items-center px-4 font-thin text-white">{title}</h1>
+    <h1 className="flex items-center px-4 font-thin text-white ">{title}</h1>
   );
 };
 
 const Navigation: React.FC<NavbarProps> = ({ items }) => {
   return (
     <>
-      <div className="flex rounded-full bg-zinc-800 p-3">
+      <div className="flex rounded-full bg-zinc-800 p-3 ">
         <ul
           className="flex list-none flex-col font-thin sm:flex-row"
           key="navbar"
         >
           {items.map((item, idx) => {
             return (
-              <Link to={item.url}>
+              <Link to={item.url} key={idx}>
                 <li
                   key={idx}
                   className="px-4 py-1 text-white transition-all hover:rounded-full hover:bg-white hover:text-zinc-800"
@@ -129,5 +147,57 @@ const SubNav = () => {
         </ul>
       </li>
     </>
+  );
+};
+
+const MobileNavBtn: React.FC<MobileNavProps> = ({ isOpen, setIsOpen }) => {
+  return (
+    <>
+      <div
+        className="rounded bg-white p-1"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        <Bars3Icon className="size-10 text-zinc-800" />
+      </div>
+    </>
+  );
+};
+
+const MobileNav: React.FC<MobileNavProps> = ({ setIsOpen }) => {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-white">
+      <ul className="list-none">
+        {navItems.map((item, idx) => {
+          return (
+            <Link to={item.url} key={idx}>
+              <li
+                className="p-4 text-center"
+                onClick={() => {
+                  setIsOpen(false);
+                }}
+              >
+                {item.title}
+              </li>
+            </Link>
+          );
+        })}
+        {subNavItems.map((item, idx) => {
+          return (
+            <Link to={item.url} key={idx}>
+              <li
+                className="p-4 text-center"
+                onClick={() => {
+                  setIsOpen(false);
+                }}
+              >
+                {item.title}
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
